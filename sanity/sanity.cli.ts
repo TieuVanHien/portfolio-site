f35e1c836fc892144d9b94202d8dc1fb6fd92869
Van Hien <hiench2001@gmail.com>
2023-04-07 14:51:21 -0600

    removed sanity

--- sanity/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'bz3yi40h',
-    dataset: 'portfolio'
-  }
-})
--- sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import skills from "./skills";
-import logo  from "./logo";
-import projects from "./projects";
-
-export const schemaTypes = [skills, logo, projects]
