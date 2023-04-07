b1517c36b0c7b87d1d5f3d5ee4e2345bc312b430
Van Hien <hiench2001@gmail.com>
2023-04-07 15:40:36 -0600

    added nav bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Nav = () => {
   return (
-    <nav>
+    <nav className="flex justify-between align-center">
       <div className="logo">
         <Image
           src="https://portfolio-site01.s3.us-west-2.amazonaws.com/logo.png"
           alt="logo"
+          className="w-40"
           width={100}
           height={50}
         />
       </div>
-      {/* other navigation links */}
+      <div className="link flex justify-between w-64 items-center">
+        <Link href="/" className="text-white">
+          Home
+        </Link>
+        <Link href="/" className="text-white">
+          Project
+        </Link>
+        <Link href="/" className="text-white">
+          Skills
+        </Link>
+        <Link href="/" className="text-white">
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
