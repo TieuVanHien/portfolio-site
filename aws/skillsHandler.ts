fbbec92b8973c786211f6de3aa77bb39e6b3891e
Van Hien Tieu <hiench2001@gmail.com>
2023-04-12 09:15:36 -0600

    added region for skills image fetching

--- aws/skillsHandler.ts
+++ aws/skillsHandler.ts
@@ -3,6 +3,7 @@ import AWS from 'aws-sdk';
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  region: 'us-west-2',
 });
 
 const bucketParams = {
