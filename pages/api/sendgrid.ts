985dfb2f25e82a07d7c8b8086416da94c96fba12
Van Hien <hiench2001@gmail.com>
2023-05-01 21:15:05 -0600

    added sendgrid api handler

--- pages/api/sendgrid.ts
+++ pages/api/sendgrid.ts
@@ -1,33 +1,36 @@
-import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+import sgMail from '@sendgrid/mail';
 
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+if (SENDGRID_API_KEY) sgMail.setApiKey(SENDGRID_API_KEY);
 interface EmailData {
   name: string;
   email: string;
   message: string;
+  success: boolean;
 }
 
-export const sendEmail = async ({ name, email, message }: EmailData) => {
-  const apiKey = process.env.SENDGRID_API_KEY;
-  const url = 'https://api.sendgrid.com/v3/mail/send';
-  const headers = {
-    Authorization: `Bearer ${apiKey}`,
-    'Content-Type': 'application/json'
-  };
-  const data = {
-    personalizations: [
-      {
-        to: [{ email: 'tieuvanhien01@gmail.com' }], // Replace with your own email address
-        subject: 'New message from your website!'
-      }
-    ],
-    from: { email: email, name: name },
-    content: [{ type: 'text/plain', value: message }]
-  };
-
-  try {
-    await axios.post(url, data, { headers: headers });
-    console.log('Email sent successfully!');
-  } catch (error) {
-    console.error(error);
+export const SendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const {
+      name,
+      email,
+      message
+    }: { name: EmailData; email: EmailData; message: EmailData } = req.body;
+    const body = {
+      to: 'tieuvanhien01@gmail.com',
+      from: 'tieuvanhien01@gmail.com',
+      subject: `${name} sent you a message`,
+      text: `Email => ${email}`,
+      html: `<p>${message}</p>`
+    };
+    try {
+      await sgMail.send(body);
+      res.status(200).json({ success: true });
+    } catch (err) {
+      res.status(200).json({ success: false });
+    }
   }
 };
+
+export default SendEmail;
