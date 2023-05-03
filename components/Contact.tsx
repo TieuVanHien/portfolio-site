ba20e7a440ea4395711e7d035c849fc5a52e3c2c
Van Hien <hiench2001@gmail.com>
2023-05-02 21:46:10 -0600

    removed unused dependencies and update validation

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,31 +1,23 @@
 import React, { useState } from 'react';
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  Textarea,
-  FormErrorMessage
-} from '@chakra-ui/react';
 
 export const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('Required');
-  const [valid, setValid] = useState(false);
-  const [submit, setSubmit] = useState(false);
+  const [disable, setDisable] = useState(true);
 
-  const handleValid = (e: any) => {
-    if (!name || !email || !message) {
-      setValid(true);
-      setSubmit(false);
-    } else if (!valid) {
-      setValid(false);
-      setSubmit(true);
+  const handleValid = () => {
+    if (name && email && message) {
+      setDisable(true);
+    } else {
+      setDisable(false);
     }
   };
+
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+    handleValid();
+
     try {
       const res = await fetch('/api/sendgrid', {
         method: 'POST',
@@ -47,55 +39,51 @@ export const Contact = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <FormControl className="text-black" isInvalid={valid} isRequired>
-          <FormLabel className="text-white">Name</FormLabel>
-          <Input
-            type="text"
-            id="name"
-            name="name"
-            errorBorderColor="red.500"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            onSubmit={handleValid}
-          />
-          <FormErrorMessage className="text-white"></FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={valid} className="text-black" isRequired>
-          <FormLabel className="text-white" htmlFor="email">
-            Email
-          </FormLabel>
-          <Input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            onSubmit={handleValid}
-          />
-          <FormErrorMessage className="text-white">
-            {errorMessage}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={valid} className="text-black" isRequired>
-          <FormLabel className="text-white" htmlFor="message">
-            Message
-          </FormLabel>
-          <Textarea
-            id="message"
-            name="message"
-            className="text-black"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onSubmit={handleValid}
-          />
-          <FormErrorMessage className="text-white">
-            {errorMessage}
-          </FormErrorMessage>
-        </FormControl>
-        <button disabled={submit} onClick={handleSubmit}>
-          Submit
-        </button>
+      <form className="flex flex-col" onSubmit={handleSubmit}>
+        <label className="text-white">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label className="text-white" htmlFor="email">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label className="text-white" htmlFor="message">
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          className="text-black"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+
+        {email && name && message ? (
+          <button className="button" type="submit" value="submit">
+            Submit
+          </button>
+        ) : (
+          <button
+            className="button"
+            type="submit"
+            value="submit"
+            disabled={disable}
+          >
+            Submit
+          </button>
+        )}
       </form>
     </div>
   );
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,49 +1,19 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
 import { Nav } from './Nav';
-import { motion, Variants } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
-  const [ref, inView] = useInView({
-    threshold: 0.1, // trigger animation when the component is 10% visible
-    triggerOnce: true // only trigger the animation once
-  });
-
-  const introHeaderVariants: Variants = {
-    hidden: {
-      opacity: 0,
-      y: -20
-    },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.8,
-        ease: 'easeOut'
-      }
-    }
-  };
-
   return (
     <div className="layout">
       <Head>
         <title>Van Hien Tieu | Software Developer</title>
       </Head>
       <Nav scrollTarget="" />
-      <motion.main
-        className="flex justify-center items-center"
-        ref={ref}
-        variants={introHeaderVariants}
-        initial="hidden"
-        animate={inView ? 'visible' : 'hidden'}
-      >
-        {children}
-      </motion.main>
+      <div className="flex justify-center items-center">{children}</div>
     </div>
   );
 };
