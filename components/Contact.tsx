f71d051505107acf840ecfddc1cc3da8ba2d3321
Van Hien <hiench2001@gmail.com>
2023-10-12 11:46:12 -0600

    fix: updated icon

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -64,16 +64,46 @@ export const Contact = () => {
           <div className="social-info">
             <Link
               href={`tel:${phone}`}
-              className="phone flex justify-start items-center mb-2"
+              className="phone icon flex justify-start items-center mb-2"
             >
-              <FontAwesomeIcon icon={faPhone} />
+              <div className="phone-icon">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="phone"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"
+                  ></path>
+                </svg>
+              </div>
               <p className="text-lg ml-6">{phone}</p>
             </Link>
             <Link
               href={`mailto:${emailAdr}`}
-              className="email flex justify-start items-center"
+              className="email icon flex justify-start items-center"
             >
-              <FontAwesomeIcon icon={faEnvelope} />
+              <div className="email-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2" />
+                  <polyline points="8 10 12 14 16 10" />
+                </svg>
+              </div>
               <p className="text-lg ml-6"> {emailAdr}</p>
             </Link>
           </div>
