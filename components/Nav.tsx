f71af449221eb4946f03f3b1f16473ba2a0e4b29
Van Hien <hiench2001@gmail.com>
2023-10-13 11:22:32 -0600

    fix: update social icon on nav bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { github, linkedin } from '@/public';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 interface NavProps {
   scrollTarget: string;
@@ -43,34 +45,24 @@ export const Nav: React.FC<NavProps> = () => {
       </div>
       <div className="contact-icon flex justify-center items-center mr-8">
         <Link
-          className="icon flex justify-center"
+          className="icon flex justify-center items-center"
           href="https://www.linkedin.com/in/van-hien-tieu-4532041b7/"
           target="_blank"
         >
-          {/* <Image src={linkedin} alt="icon" /> */}
+          <LinkedInIcon fontSize="large" />
         </Link>
         <Link
-          className="icon flex justify-center"
+          className="icon flex justify-center items-center"
           href="https://github.com/TieuVanHien"
           target="_blank"
         >
-          <Image src={github} alt="github icon" />
+          <GitHubIcon fontSize="large" />
         </Link>
-        <Link className="icon" href={`mailto:${emailAdr}`}>
-          <>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <rect x="2" y="3" width="20" height="14" rx="2" ry="2" />
-              <polyline points="8 10 12 14 16 10" />
-            </svg>
-          </>
+        <Link
+          className="icon flex justify-center items-center"
+          href={`mailto:${emailAdr}`}
+        >
+          <EmailIcon fontSize="large" />
         </Link>
       </div>
     </nav>
