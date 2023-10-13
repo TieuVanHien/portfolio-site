b43126e50cafca63c39c643009fe16976704320b
Van Hien <hiench2001@gmail.com>
2023-10-13 11:51:08 -0600

    style: added parallax scrolling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export const Footer = () => {
-  return <div>© Van Hien Tieu. All Rights Reserved 2023. </div>;
+  return (
+    <div className="footer">© Van Hien Tieu. All Rights Reserved 2023. </div>
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,21 @@ export default function App() {
   return (
     <ParallaxProvider>
       <div className="wrapper flex flex-col items-center justify-center p-24">
-        <Parallax translateY={[1, -30]} speed={-5}>
+        <Parallax translateY={[0, 0]} speed={-5}>
           <Home />
         </Parallax>
-        <Parallax translateY={[-15, 25]} speed={10}>
+        <Parallax translateY={[20, -30, 'easeOutQuint']} speed={10}>
           <Skills />
         </Parallax>
-        <Parallax translateY={[-10, 15]} speed={-5}>
+        <Parallax translateY={[-10, 15, 'easeOutQuad']} speed={20}>
           <Project projects={projects} />
         </Parallax>
-        <Parallax translateY={[-25, 35]} speed={10}>
+        <Parallax translateY={[-25, 20, 'easeOutQuint']} speed={10}>
           <Contact />
         </Parallax>
-        <Footer />
+        <Parallax translateX={[-5, 0, 'easeOutQuint']} speed={10}>
+          <Footer />
+        </Parallax>
       </div>
     </ParallaxProvider>
   );
