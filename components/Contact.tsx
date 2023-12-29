aacb579fe3c91226617bde41ced74d7483c8e942
Van Hien <hiench2001@gmail.com>
2023-12-28 21:49:58 -0700

    fix: resolved warning

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -94,9 +94,9 @@ export const Contact = () => {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
                   <rect x="2" y="3" width="20" height="14" rx="2" ry="2" />
                   <polyline points="8 10 12 14 16 10" />
