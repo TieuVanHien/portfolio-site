f4678d71262e61f9d95af868d1f97551c6317f26
Van Hien <hiench2001@gmail.com>
2023-12-29 14:26:52 -0700

    removed react scroll and warning

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,17 @@
 import { Home, Skills, Project, Contact } from '@/components';
 import { Footer } from '@/components/Footer';
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import { projects } from '@/project/project';
 
 export default function App() {
   return (
-    <ParallaxProvider>
+    <div>
       <div className="wrapper flex flex-col items-center justify-center p-24">
-        <Parallax translateY={[0, 0]} speed={-5}>
-          <Home />
-        </Parallax>
-        <Parallax translateY={[20, -30, 'easeOutQuint']} speed={10}>
-          <Skills />
-        </Parallax>
-        <Parallax translateY={[-10, 15, 'easeOutQuad']} speed={20}>
-          <Project projects={projects} />
-        </Parallax>
-        <Parallax translateY={[-25, 20, 'easeOutQuint']} speed={10}>
-          <Contact />
-        </Parallax>
-        <Parallax translateX={[-5, 0, 'easeOutQuint']} speed={10}>
-          <Footer />
-        </Parallax>
+        <Home />
+        <Skills />
+        <Project projects={projects} />
+        <Contact />
+        <Footer />
       </div>
-    </ParallaxProvider>
+    </div>
   );
 }
