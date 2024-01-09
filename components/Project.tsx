937b44752a5220d8a2d9c40a5aadac6d9c730bd7
Van Hien <hiench2001@gmail.com>
2024-01-09 13:18:55 -0700

    style: updated style for projects section in desktop and mobile version

--- components/Project.tsx
+++ components/Project.tsx
@@ -21,63 +21,60 @@ interface ProjectProps {
 
 export const Project = ({ projects }: ProjectProps) => {
   return (
-    <section id="projects" className="projects flex justify-evenly">
-      <div className="heading flex justify-start items-center">
-        <h1>My Projects</h1>
-        <div className="line"></div>
+    <section
+      id="projects"
+      className="projects flex flex-col justify-evenly w-full h-auto mt-16 lg:w-full lg:h-auto lg:flex lg:flex-col lg:items-center lg:justify-evenly"
+    >
+      <div className="heading mb-8 flex justify-start items-center lg:h-1/10 lg:w-full lg:mb-8 lg:flex lg:justify-start lg:items-center">
+        <h1 className="text-2xl lg:text-4xl">My Projects</h1>
+        <div className="line ml-4 w-3/5 lg:w-1/4 lg:ml-4"></div>
       </div>
       {projects.map((project) => (
-        <>
-          <div
-            className="frame flex justify-between items-center m-16"
-            key={project._id}
-          >
-            <Image
-              className="image"
-              src={project.img.src}
-              alt={project.title}
-              width={600}
-              height={600}
-            />
-            <div className="details flex flex-col justify-between items-start">
-              <div className="project-heading flex justify-between items-center">
-                <h1>{project.title}</h1>
-                <div className="link">
-                  <Link
-                    target="blank"
-                    className="repo-link mr-2"
-                    href={project.repo}
-                  >
-                    <GitHubIcon sx={{ fontSize: 25 }} />
-                  </Link>
-                  <Link
-                    target="blank"
-                    className="repo-link"
-                    href={project.repo}
-                  >
-                    <OpenInNew sx={{ fontSize: 25 }} />
-                  </Link>
-                </div>
+        <div
+          className="frame border-solid border-2 rounded-md border-slate-500 p-4 mb-14 flex justify-center flex-col items-center lg:border-none lg:flex-row lg:w-full h-full lg:flex lg:justify-between lg:items-center lg:m-16"
+          key={project._id}
+        >
+          <Image
+            className="image w-full h-52 lg:rounded-lg lg:w-1/2 lg:h-80"
+            src={project.img.src}
+            alt={project.title}
+            width={600}
+            height={600}
+          />
+          <div className="details w-full h-52 mt-8 flex flex-col justify-evenly lg:border-solid lg:border-2 lg:border-slate-500 lg:m-0 lg:rounded-lg lg:h-72 lg:p-8 lg:w-2/5 lg:flex lg:flex-col lg:justify-between lg:items-start">
+            <div className="project-heading flex justify-between items-center lg:w-full lg:flex lg:justify-between lg:items-center">
+              <h1 className="text-2xl lg:text-3xl">{project.title}</h1>
+              <div className="link">
+                <Link
+                  target="blank"
+                  className="repo-link mr-2 lg:mr-2"
+                  href={project.repo}
+                >
+                  <GitHubIcon sx={{ fontSize: 25 }} />
+                </Link>
+                <Link target="blank" className="repo-link" href={project.repo}>
+                  <OpenInNew sx={{ fontSize: 25 }} />
+                </Link>
               </div>
-              <h2>{project.desc}</h2>
-              {project.techstack && (
-                <div className="flex">
-                  {project.techstack.map((tech) => (
-                    <div className="flex" key={tech.name}>
-                      <Image
-                        className="icon ml-2"
-                        src={tech.src}
-                        alt={tech.name}
-                        width={35}
-                        height={35}
-                      />
-                    </div>
-                  ))}
-                </div>
-              )}
             </div>
+            <h2 className="text-base lg:text-lg">{project.desc}</h2>
+            {project.techstack && (
+              <div className="lg:flex flex">
+                {project.techstack.map((tech) => (
+                  <div className="lg:flex" key={tech.name}>
+                    <Image
+                      className="icon w-8 h-8 ml-2 lg:ml-2"
+                      src={tech.src}
+                      alt={tech.name}
+                      width={35}
+                      height={35}
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-        </>
+        </div>
       ))}
     </section>
   );
