fa4133fa1a4337057954623d173d13a41c684b3a
Van Hien <hiench2001@gmail.com>
2024-01-08 22:08:26 -0700

    style: update style for home section in mobile version

--- components/Home.tsx
+++ components/Home.tsx
@@ -6,12 +6,14 @@ export const Home = () => {
   return (
     <section
       id="home"
-      className="home pt-32 lg:flex-row mob:flex-col h-screen w-full lg:flex"
+      className="home pt-28 lg:flex-row mob:flex-col h-screen w-full lg:flex"
     >
-      <div className="right h-1/2 w-full flex justify-center items-center lg:h-full lg:w-7/12 lg:flex lg:justify-start lg:items-center lg:pb-24">
-        <div className="card h-72 flex flex-col justify-between lg:font-mono lg:flex-col lg:flex lg:justify-between lg:text-2xl">
-          <div className="text ">
-            <h1>Welcome to my portfolio site!</h1>
+      <div className="right h-fit w-full flex justify-center items-center lg:h-full lg:w-7/12 lg:flex lg:justify-start lg:items-center lg:pb-24">
+        <div className="card w-4/5 p-8 h-56 flex flex-col justify-between lg:p-8 lg:font-mono lg:h-3/5 lg:flex-col lg:flex lg:justify-between lg:text-2xl">
+          <div>
+            <h1 className="lg:text-3xl text-xl mb-4">
+              Welcome to my portfolio site!
+            </h1>
             <TypingComponent />
           </div>
           <div className="info flex justify-end lg:flex lg:justify-end lg:text-lg">
@@ -19,7 +21,7 @@ export const Home = () => {
           </div>
         </div>
       </div>
-      <div className="left lg:w lg:flex lg:justify-center lg:pl-24">
+      <div className="left w-full h-80 flex justify-center items-center lg:w lg:flex lg:justify-center lg:pl-24">
         <RoboModel />
       </div>
     </section>
