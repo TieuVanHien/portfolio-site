c96deabae37689d6a0a32a1eaeaa39448b6d11ae
Van Hien <hiench2001@gmail.com>
2024-01-09 13:27:50 -0700

    updated card style in mobile version

--- components/Home.tsx
+++ components/Home.tsx
@@ -8,8 +8,8 @@ export const Home = () => {
       id="home"
       className="home pt-28 h-fit w-full lg:h-screen lg:flex-row lg:flex"
     >
-      <div className="right h-fit w-full flex justify-center items-center lg:h-full lg:w-7/12 lg:flex lg:justify-start lg:items-center lg:pb-24">
-        <div className="card w-4/5 p-8 h-56 flex flex-col justify-between lg:p-8 lg:font-mono lg:h-3/5 lg:flex-col lg:flex lg:justify-between lg:text-2xl">
+      <div className="right p-4 h-fit w-full flex justify-center items-center lg:h-full lg:w-7/12 lg:flex lg:justify-start lg:items-center lg:pb-24">
+        <div className="card w-full p-8 h-56 flex flex-col justify-between lg:p-8 lg:font-mono lg:h-3/5 lg:flex-col lg:flex lg:justify-between lg:text-2xl">
           <div>
             <h1 className="lg:text-3xl text-xl mb-4">
               Welcome to my portfolio site!
