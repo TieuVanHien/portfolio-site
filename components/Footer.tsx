82b856bbf5685a1d8e556d4db2e9dc6b4c1f2c51
Van Hien <hiench2001@gmail.com>
2024-01-16 22:04:33 -0700

    style: updated style for home section and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 export const Footer = () => {
   return (
     <div className="footer text-sm lg:text-base flex justify-center items-center h-24">
-      © Van Hien Tieu. All Rights Reserved 2023.{' '}
+      <span className="text-sm lg:text-base">© Made by </span>
+      <h1 className="ml-2 text-sm lg:text-base">Van Hien Tieu</h1>
     </div>
   );
 };
