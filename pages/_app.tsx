89102d1add85bd31821aaa0a66f434aef08fa198
Van Hien <hiench2001@gmail.com>
2024-06-23 11:14:57 -0600

    clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Layout } from '@/components';
 import '@/styles/globals.scss';
-import Head from 'next/head';
 
 interface AppProps {
   Component: React.ComponentType<any>;
